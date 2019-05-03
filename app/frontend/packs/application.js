bc7c4909e8cb33fbc868789c0cde2dfaa3a16347
sunakan <sunakan03@gmail.com>
2019-05-04 01:26:11 +0900

    [Add] assets類を全てwebpackで管理する

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -5,11 +5,14 @@
 
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
+require("../javascripts/helloworld")
+require("../javascripts/h1red")
 
+// css
+require("../stylesheets/application")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+const images = require.context('../images', true)
