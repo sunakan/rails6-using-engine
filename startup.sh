24226e0c0784d50e86f6d5f6829e737435db20ac
sunakan <sunakan03@gmail.com>
2019-05-05 18:07:46 +0900

    [Add] startup.shにbin/webpackを追加

--- startup.sh
+++ startup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+#bundle update blog_engine
 bundle check || bundle install
 
 bundle exec rake db:create
 bundle exec rake db:migrate
+bin/webpack
 
 bundle exec puma -C config/puma.rb
